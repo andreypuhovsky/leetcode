22a9e07939fce3b85372e054c147cab947f01721
Andrey Puhovsky <andrey@puhovsky.com>
2024-02-04 13:26:15 +0100

    Edit solution for "[4]Median of Two Sorted Arrays" problem in TypeScript
    Refactor solution to handle empty input arrays

--- leetcode/editor/en/[4]Median of Two Sorted Arrays.ts
+++ leetcode/editor/en/[4]Median of Two Sorted Arrays.ts
@@ -1,15 +1,30 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
-function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-    const nums = [...nums1, ...nums2]
-        .sort((a, b) => a - b)
+function medianOfArray(nums: number[], length: number): number{
+    const middleIdx = ~~(length / 2)
 
-    const middleIdx = ~~(nums.length / 2)
-
-    if (nums.length % 2 === 1) {
+    if (length % 2 === 1) {
         return nums[middleIdx]
     }
 
     return (nums[middleIdx - 1] + nums[middleIdx]) / 2
+}
+
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+    const nums1Length = nums1.length
+    const nums2Length = nums2.length
+
+    if (nums1Length === 0) {
+        return medianOfArray(nums2, nums2Length)
+    }
+
+    if (nums2Length === 0) {
+        return medianOfArray(nums1, nums1Length)
+    }
+
+    const nums = [...nums1, ...nums2]
+        .sort((a, b) => a - b)
+
+    return medianOfArray(nums, nums.length)
 };
 //leetcode submit region end(Prohibit modification and deletion)
