4c40b35006c0f454d6f7353b7f0f3727b4f25cd6
Andrey Puhovsky <andrey@puhovsky.com>
2024-02-04 13:26:15 +0100

    Edit solution for "[2]Add Two Numbers" problem in TypeScript

--- leetcode/editor/en/[2]Add Two Numbers.ts
+++ leetcode/editor/en/[2]Add Two Numbers.ts
@@ -16,27 +16,31 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
     let currentL1 = l1,
         currentL2 = l2
 
-    let overflow = 0
+    let overflow = false
 
     const resultL = new ListNode()
     let currentL = resultL
 
+    // Loop through the lists until both are null and there is no overflow
     while (currentL1 || currentL2 || overflow) {
         let prenumber =
             (currentL1?.val || 0) +
             (currentL2?.val || 0) +
-            (overflow)
+            (overflow ? 1 : 0)
 
-        overflow = 0
+        overflow = false
 
+        // If the number is 10 or more, we need to set the overflow to the next number
         let number = prenumber % 10
         if (number !== prenumber) {
-            overflow = 1
+            overflow = true
         }
 
+        // Add the number to result and move to the next
         currentL.next = new ListNode(number)
         currentL = currentL.next
 
+        // Move to the next nodes on both lists
         currentL1 = currentL1?.next
         currentL2 = currentL2?.next
     }
