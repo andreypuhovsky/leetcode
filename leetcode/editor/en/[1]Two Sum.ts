f612e65a36c75695f78ee6053054e0788c799b8c
Andrey Puhovsky <andrey@puhovsky.com>
2024-02-04 13:26:15 +0100

    Edit solution for "[1]Two Sum" problem in TypeScript
    Refactor "[1]Two Sum" solution for improved efficiency in TypeScript

--- leetcode/editor/en/[1]Two Sum.ts
+++ leetcode/editor/en/[1]Two Sum.ts
@@ -1,16 +1,22 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 function twoSum(nums: number[], target: number): number[] {
-    for (const num1Idx in nums) {
-        const num1 = nums[num1Idx];
+    // We iterate through the array using the index and for each number
+    for (const num1IdxString in nums) {
+        // We convert the index to a number as we expect it to be a number
+        const num1Idx = +num1IdxString;
 
-        for (const relIdx in nums.slice(+num1Idx)) {
-            const num2Idx = +num1Idx + +relIdx + 1;
-            const num2 = nums[num2Idx];
+        // We check if the difference between the target and
+        // the number is in the array.
+        const num2 = target - nums[num1Idx];
 
-            if (num1 + num2 === target) {
-                return [+num1Idx, +num2Idx];
-            }
+        // We start searching from the index after the current number
+        // to avoid using the same index twice.
+        const num2Idx = nums.indexOf(num2, num1Idx + 1);
+
+        // If it is, we return the constituents' indices.
+        if (num2Idx !== -1) {
+            return [num1Idx, num2Idx];
         }
     }
 };
